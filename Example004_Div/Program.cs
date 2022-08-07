b5e282573f085461fd88f5477f9d2bfbaf6f4117
rustem <tkanapyanov@gmail.com>
2022-08-07 17:58:30 +0600

    correct text

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+//Типы данных int- 32-разрядное число со знаком
+//Типы данных double- 64-разрядное число со знаком
+//Строки String- от 0 до много символов от 4 до 2гб
+//bool true и false = истина и лож 1 байт
+//Классические арифметические операторы + - / * % ()
+//Случайности new Random().Next(min, max) Даст случайное целое число от min до max-1
+//[min, max-1] или [min, max]
